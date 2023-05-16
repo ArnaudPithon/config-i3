3c6578925e059bbec2fb76bb49900ca554ee2a51
bildoon <bildoon@free.fr>
2023-05-16 11:11:21 +0200

    Début d'adaptation pour l'utilisation d'alacritty en remplacement de urxvt

--- session.sh
+++ session.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env dash
 # vim: ft=sh
 # $Id$
-# Last modified: 2018-11-04 18:53:02+0100
+# Last modified: 2023-05-16 11:09:14+0200
 
-_term() {
-	APP=$@
-	tmux ls 2> /dev/null | grep "^$APP:" &&\
-		command urxvtc -name $APP -e tmux attach -t $APP
-	command urxvtc -name $APP -e tmux new -s $APP
-	}
 
-urxvtc -name mail
+if [ -x /bin/alacritty ]
+  then
+    _term() {
+      APP=$@
+      tmux ls 2> /dev/null | grep "^$APP:" &&\
+        command alacritty --class $APP,alacritty -e tmux attach -t $APP &
+      command alacritty --class $APP,alacritty -e tmux new -s $APP &
+    }
+  elif [ -x /usr/bin/urxvtc ]
+  then
+    _term() {
+      APP=$@
+      tmux ls 2> /dev/null | grep "^$APP:" &&\
+        command urxvtc -name $APP -e tmux attach -t $APP
+      command urxvtc -name $APP -e tmux new -s $APP
+    }
+  else
+    _term() {
+      APP=$@
+      tmux ls 2> /dev/null | grep "^$APP:" &&\
+        command xterm -name $APP -e tmux attach -t $APP
+      command xterm -name $APP -e tmux new -s $APP
+    }
+fi
+
+alacritty --class,alacritty mail &
 
 _term v
 _term main
